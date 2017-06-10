a0ed41454a800240662fadcdafc292bb3a9606a3
Masud <mr.masudrana00@gmail.com>
2017-06-11 01:53:10 +0600

    File updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,6 @@
 (function($) {
     'use strict';
 
-    $('#menu-trigger-id').on('click', function(e) {
-        var $this = $(this);
-        $this.toggleClass('active');
-        e.preventDefault();
-    });
-    var wow = new WOW({
-        boxClass: 'wow', // animated element css class (default is wow)
-        animateClass: 'animated', // animation css class (default is animated)
-        offset: 0, // distance to the element when triggering the animation (default is 0)
-        mobile: true, // trigger animations on mobile devices (default is true)
-        live: true, // act on asynchronously loaded content (default is true)
-    });
-    wow.init();
+    // All JavaScript pluging initialization code here
 
 })(jQuery)
