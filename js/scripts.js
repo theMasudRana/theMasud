c07c2647582bfd4b5542d3f9c167735972f36c32
Masud <mr.masudrana00@gmail.com>
2017-07-06 00:27:36 +0600

    Portfolio section added

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,46 @@
     });
     wow.init();
 
+
+
+    // Portfolio section 
+    $('.portfolio-section').imagesLoaded(function() {
+        // Portfolio activation
+        var $grid = $('.portfolio-grid').isotope({
+            itemSelector: '.portfolio-item',
+            percentPosition: true,
+        });
+
+        // Portfolio filtering activation
+        $('.portfolio-filter li a').on('click', function() {
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({ filter: filterValue });
+        });
+
+        // filter menu class addition  
+        $('.portfolio-filter li').on('click', function(event) {
+            $(this).siblings('.active').removeClass('active');
+            $(this).addClass('active');
+            event.preventDefault();
+        });
+
+        /*--------------------------------------------------
+        LOAD MORE BUTTON FOR PORTFOLIO
+        ---------------------------------------------------*/
+        $('.pagination-area').on('click', '.lode-more', function(event) {
+            $('.portfolio-item').removeClass('hidden');
+            var $grid = $('.portfolio-grid').isotope();
+            $(this).hide();
+            $('.pagination-area .load-more').append('<a class="button" href="javascript:void(0)">No More Items</a>')
+            event.preventDefault();
+        });
+    });
+
+
+
+
+
+
+
+
 })(jQuery)
